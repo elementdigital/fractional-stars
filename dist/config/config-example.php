110a667d0079d56107ba044375f501da01dd4df1
elementdigital <gituser@elementdigital.com>
2019-02-17 17:11:27 -0800

    add config example file

---
+++ dist/config/config-example.php
@@ -0,0 +1,13 @@
+<?php
+//Database server, often "localhost"
+define("FS_DB_SERVER", "YOUR-DB-SERVER");
+
+//Database user name
+define("FS_DB_USER", "YOUR-DB-USER");
+
+//Database user password
+define("FS_DB_PASS", "YOUR-DB-PASSWORD");
+
+//Database name
+define("FS_DB_NAME", "YOUR-DB-NAME");
+?>
